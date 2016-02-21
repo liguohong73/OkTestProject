5344952e7e927b2b3ae721f4f29ed72a91aae0d6
liguohong73 <1451687525@qq.com>
2016-02-21 16:56:34 +0800

    eeed

--- app/src/main/java/com/admin/testproject/MainActivity.java
+++ app/src/main/java/com/admin/testproject/MainActivity.java
@@ -28,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        Log.e("rrrrr","rrrrrrrrrrrrrrr");
+        Log.e("rrrrr","rrrrrr");
     }
 
     @Override
