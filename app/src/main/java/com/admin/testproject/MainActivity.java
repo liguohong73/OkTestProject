635af77b19b858cc8e9a2e491478705df49387df
liguohong73 <1451687525@qq.com>
2016-02-21 16:37:45 +0800

    eeed

--- app/src/main/java/com/admin/testproject/MainActivity.java
+++ app/src/main/java/com/admin/testproject/MainActivity.java
@@ -42,6 +42,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
